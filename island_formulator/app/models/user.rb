01ecb850449d02cfa07d2d95980f2c3c2f203fdf
Aziza Gentles <azizagentles@gmail.com>
2026-03-01 07:37:08 -0800

    feat(batches): add batch logging for recipes

--- island_formulator/app/models/user.rb
+++ island_formulator/app/models/user.rb
@@ -4,7 +4,8 @@
   has_many :sessions, dependent: :destroy
   has_many :ingredients, dependent: :destroy
   has_many :recipes, dependent: :destroy # week3
-  has_many :inventory_items, dependent: :destroy # week4
+  has_many :inventory_items, dependent: :destroy # week4 p1
+  has_many :batches, dependent: :destroy # week 4 p2
 
   # Add validations for better error messages
   validates :email_address, presence: true, uniqueness: { case_sensitive: false }
